20c1ad070508b15664d861d94462ba05fa2d5338
taiyangxin <52385945+taiyangxin@users.noreply.github.com>
2019-07-26 14:52:05 -0700

    Create wind3.py

---
+++ wind3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb
+cgitb.enable()
+
+input_data=cgi.FieldStorage()
+
+print 'Content-Type:text/html' #HTML is following
+print "aaa"
+print "aaaddd   dd"
+print "aaaddd   1dd"
